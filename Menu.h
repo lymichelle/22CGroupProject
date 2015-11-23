fded4384dd769e9dbef0ce8496d13f106c0a04d1
DestinyCookie <jmldwyer@gmail.com>
2015-11-22 18:39:00 -0800

    Added Menu header and implementation
    barebones; needs fleshing out

---
+++ Menu.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+void caseSearch();
+void caseList();
+void caseAdd();
+void caseDelete();
+void caseUndo();
+void caseWriteFile();
+void caseStatistic();
+bool outFileValid(ofstream &outFile);
