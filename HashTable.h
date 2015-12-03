9dc4d25403051d0c7c887f1871ad69b6a48c8069
njavs <nausheen.sujela@gmail.com>
2015-12-02 18:08:32 -0800

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -3,7 +3,8 @@
 #include "HashNode.h"
 #include "Mineral.h"
 #include <string>
-
+#include <ctime>
+#include <cmath>
 using namespace std;
 template<class T>
 class Hash
@@ -13,13 +14,21 @@ class Hash
         virtual ~Hash();
         HashNode<T> **htable, **top;
         int tableSize = 25;
+        int numOfBlocksInHashFilled = 0;
+        int numOfCollisions = 0;
 
         int hashFunc(string key);
+        int rehashFunc(string key, string response);
+        void rehashInsert(string key, T data);
         void insert(string key, T data);
         bool remove(string key);
         int Search(string key);
         bool getEntry(string key, T & data);
         void traverseHashTable(void visit(HashNode<T>*));
+        bool isPrime(int x);
+        int resizeTable();
+        void getLoadFactor();
+        void showStatistics();
     protected:
     private:
 };
@@ -52,23 +61,14 @@ int Hash<T>::hashFunc(string key)
     for (int i = 0; i < key.length(); i++){
         sum += (int)key[i];
     }
-    return sum % tableSize;
-}
-
-/*
-//assigns index to key.
-template<class T>
-int Hash<T>::hashFunc(string key)
-{
-
-    int n;
-    for(int i = 0; key.length(); i++){
-        n += key.at(i);
+    string power = "Hello Delia!";
+    int powerSum = 0;
+    for (int i = 0; i < power.length(); i++){
+        powerSum += (int)power[i];
     }
 
-    return stoi(key) % tableSize;
+    return (sum^(powerSum)) % tableSize;
 }
-*/
 
 //insert key into hash table.
 template<class T>
@@ -83,23 +83,20 @@ void Hash<T>::insert (string key, T data)
             entry->next = NULL;
             entry->prev = NULL;
             htable[hashIndex] = entry;
-			top[hashIndex] = entry;
+            top[hashIndex] = entry;
 
             cout << (htable[hashIndex])->getKey() << " inserted" << endl;
         }
     else
         {
-			HashNode<T> *pPrev;
-			while (entry != NULL){
-				pPrev = entry;
-				entry = entry->next;
-			}
+            while (entry != NULL)
+                entry = entry->next;
             entry = new HashNode<T>(key, data);
             entry->_dataPtr = data;
             entry->next = NULL;
-            entry->prev = pPrev;
-            pPrev->next = entry;
-            //top[hashIndex] = entry;
+            entry->prev = top[hashIndex];
+            top[hashIndex]->next = entry;
+            top[hashIndex] = entry;
             cout << key << " inserted" << endl;
     }
 }
@@ -200,17 +197,123 @@ void Hash<T>::traverseHashTable(void visit(HashNode<T>*))
         //visit it.
         if (htable[i]!=NULL){
             int j = 0;
-            //cout << "Stored @ index: " << i << ", " << j << endl;
+            cout << "Stored @ index: " << i << ", " << j << endl;
             visit(htable[i]);
 
             while (traverser->next!=NULL){
                 j++;
                 traverser = traverser->next;
-                //cout << "Stored @ index: " << i << ", " << j << endl;
+                cout << "Stored @ index: " << i << ", " << j << endl;
                 visit(traverser);
                 }
         }
     }
 }
+
+//check if number is prime.
+template <class T>
+bool Hash<T>::isPrime(int x)
+{
+  bool prime;
+  int numOfPrimes = 1;
+  int *primeNumbers;
+  int *temp;
+  primeNumbers = new int;
+  primeNumbers[0] = 2;
+
+    for (int i = 3; i <= x; i++){
+        prime = true;
+
+        for (int j = 0; j < numOfPrimes; j++){
+            if (i % primeNumbers[j] == 0){
+                prime = false; //if divisible by a compound number or another prime number, it is not prime.
+            }
+        }
+    if (prime){
+        temp = new int;
+        if (temp != primeNumbers) delete(primeNumbers);
+        primeNumbers = temp;
+        primeNumbers[numOfPrimes-1] = i;
+    }
+    }
+    return prime;
+}
+
+
+//resizing function.
+//This function increases size of table and creates new hashFunc.
+template <class T>
+int Hash<T>::resizeTable()
+{
+    int newTableSize = tableSize + 1;
+    while (isPrime(newTableSize) = 0)
+    {
+        cout << "not prime" << endl;
+        resizeTable();
+    }
+    tableSize = newTableSize;
+}
+
+template <class T>
+int Hash<T>::rehashFunc(string key, string response)
+{
+    cout << "What's your favorite ice cream flavor?" << endl;
+
+    int sum = 0;
+    for (int i = 0; i < key.length(); i++){
+        sum += (int)key[i];
+    }
+    int powerSum = 0;
+    for (int i = 0; i < response.length(); i++){
+        powerSum += (int)response[i];
+    }
+
+    return (sum^powerSum % tableSize);
+}
+
+template <class T>
+void Hash<T>::rehashInsert(string key, T data){
+
+    resizeTable();
+ int hashIndex = rehashFunc(key);
+    HashNode<T> *entry = htable[hashIndex];
+    if (entry == NULL)
+        {
+            entry = new HashNode<T>(key, data);
+            entry->_dataPtr = data;
+            entry->next = NULL;
+            entry->prev = NULL;
+            htable[hashIndex] = entry;
+            top[hashIndex] = entry;
+
+            cout << (htable[hashIndex])->getKey() << " inserted" << endl;
+        }
+    else
+        {
+            while (entry != NULL)
+                entry = entry->next;
+            entry = new HashNode<T>(key, data);
+            entry->_dataPtr = data;
+            entry->next = NULL;
+            entry->prev = top[hashIndex];
+            top[hashIndex]->next = entry;
+            top[hashIndex] = entry;
+            cout << key << " inserted" << endl;
+    }
+}
+
+template <class T>
+void Hash<T>::getLoadFactor()
+{
+    return static_cast<float>(numOfBlocksInHashFilled/tableSize);
+}
+
+template <class T>
+void Hash<T>::showStatistics()
+{
+    std::cout << "Load Factor: " << numOfBlocksInHashFilled << "/" << tableSize << std::endl;
+    std::cout << "Number of collisions: " << numOfCollisions << std::endl;
+}
+
 
 #endif // HASH_H
